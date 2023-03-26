cc05d4f94098e58d93d59a11f6262714169b7596
George <c2021423@ncl.ac.uk>
2023-03-26 14:39:44 +0100

    changed Reset script to reset cat ownership

--- Assets/Scripts/ResetDataScript.cs
+++ Assets/Scripts/ResetDataScript.cs
@@ -8,6 +8,7 @@ public class ResetDataScript : MonoBehaviour
     public void Reset()
     {
         PlayerPrefs.SetInt("coins", 0);
+        PlayerPrefs.SetInt("CAT", 0);
         PlayerPrefs.SetInt("DOG", 0);
         PlayerPrefs.SetInt("selectedChar", 0);
         PlayerPrefs.SetInt("difficulty", 1);
